bc682863a45b5a3702fc309b01b8263cf4a2c37e
Tung Bui <tungbq.info@gmail.com>
2021-10-31 23:58:42 +0700

    [server] Initialize users and posts route + basic queries

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,5 @@
 var express = require('express');
 var router = express.Router();
-require('dotenv').config();
-
-const db = require('../db');
-db.query('SELECT * FROM users').then((result) => console.log(result));
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const db = require('../db');
 
-// Create user
+// Create post
 router.post('/', async (req, res) => {
 	var date_ob = new Date();
 	var day = ('0' + date_ob.getDate()).slice(-2);
@@ -14,9 +14,27 @@ router.post('/', async (req, res) => {
 
 	try {
 		const response = await db.query(
-			'INSERT INTO users (username, password, email, created_date) VALUES ($1, $2, $3, $4) RETURNING *',
-			[req.body.username, req.body.password, req.body.email, created_date]
+			'INSERT INTO posts (title, body, user_id, author, created_date) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+			[
+				req.body.title,
+				req.body.body,
+				req.body.user_id,
+				req.body.author,
+				created_date,
+			]
 		);
+		console.log(response.rows);
+		res.status(201).json(response.rows);
+	} catch (error) {
+		console.error({ error });
+		res.status(500).json({ error });
+	}
+});
+
+// Get all posts
+router.get('/', async (req, res) => {
+	try {
+		const response = await db.query('SELECT * FROM posts');
 		console.log(response.rows);
 		res.status(201).json(response.rows);
 	} catch (error) {
