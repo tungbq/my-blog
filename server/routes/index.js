1056b1be3e25b6c7eb62b91d5b2e3a6984910e3e
Tung Bui <tungbq.info@gmail.com>
2021-10-31 21:47:28 +0700

    [server] connect to postgres database

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config();
+
+const db = require('../db');
+db.query('SELECT * FROM users').then((result) => console.log(result));
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send({ "message": "Hi, there!"});
+router.get('/', function (req, res, next) {
+	res.send({ message: 'Hi, there!' });
 });
 
 module.exports = router;
